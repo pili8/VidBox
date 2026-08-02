bf63ff217855512dffcbbffca9b788b5173c7600
Gming <pili8@163.com>
2026-08-02 21:52:14 +0800

    ci: Release 名称改用版本号 + 升版到 v3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yuafeng.videoswiper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "3.0"
+        versionCode = 11
+        versionName = "3.1"
     }
 
     signingConfigs {
